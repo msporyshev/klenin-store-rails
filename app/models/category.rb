a354e0f8aa4da8738d2fcbccd427393b0854f5c7
Max Sporyshev <coockoombra@gmail.com>
2012-03-15 20:22:03 +1100

    Upgraded categories view. Added twitter-less to categories.
    Path creates automatically on save to database. Parent category name instead of category id.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+
   belongs_to :category
   has_many :categories
   has_many :products
+
+  after_save lambda { |category|
+      category.path = category.category.nil? ? "#{category.id}." : category.category.path + "#{category.id}."
+      category.save!
+      }, :if => lambda{ |category| category.path.nil? }
 end
